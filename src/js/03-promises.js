07a78d5a2ca6776a788365ae58a1b6857616f7f5
alexandr-ni <sashanikitenko119@gmail.com>
2023-06-03 21:38:15 +0300

    bugfix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,46 +5,45 @@ Notify.init({
 });
 
 const itemsEl = document.querySelectorAll('input');
-const submitButton = document.querySelector('button');
-
-let counter = 1;
+const formSubmit = document.querySelector('.form');
 
 const amount = itemsEl[2];
-const delay = itemsEl[0];
-const newDelay = itemsEl[1];
+const delayEl = itemsEl[0];
+const stepEl = itemsEl[1];
+let position = 0;
 
-submitButton.addEventListener('click', createPromise);
+formSubmit.addEventListener('submit', stopDefAction);
 
-function createPromise(evt) {
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function stopDefAction(evt) {
   evt.preventDefault();
 
-  submitButton.removeEventListener('click', createPromise);
+  formSubmit.removeEventListener('submit', stopDefAction);
 
-  let convertIntoNumber = Number(delay.value);
-  let convertIntoNewNumber = Number(newDelay.value);
+  let delay = Number(delayEl.value);
+  const step = Number(stepEl.value);
 
-  setTimeout(function time() {
-    const shouldResolve = Math.random() > 0.3;
-
-    if (shouldResolve) {
-      Notify.success(
-        `✅ Fulfilled promise ${counter} in ${convertIntoNumber}ms`
-      );
-      counter += 1;
-      convertIntoNumber += convertIntoNewNumber;
-    } else {
-      Notify.failure(
-        `❌ Rejected promise ${counter} in ${convertIntoNumber}ms`
-      );
-      counter += 1;
-      convertIntoNumber += convertIntoNewNumber;
-    }
-    if (counter <= Number(amount.value)) {
-      setTimeout(time, convertIntoNumber);
-    }
-    if (counter > Number(amount.value)) {
-      counter = 1;
-      return submitButton.addEventListener('click', createPromise);
-    }
-  }, convertIntoNumber);
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+    position += 1;
+  }
 }
